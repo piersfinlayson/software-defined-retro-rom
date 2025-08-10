b250fb5af7358732a3c7b94ba5d59f0c63e73ad8
Piers Finlayson <piers@piers.rocks>
2025-08-10 20:03:06 +0100

    added new basic demo

--- demo/build.sh
+++ demo/build.sh
@@ -3,7 +3,10 @@ C1541="c1541"
 
 C64_BAS="intro-c64.bas"
 C64_PRG="intro-c64.prg"
+C64_BAS2="one-c64.bas"
+C64_PRG2="one-c64.prg"
 C64_OUT="sdrrc64"
+C64_OUT2="sdrr2c64"
 VIC20_BAS="intro-vic20.bas"
 VIC20_PRG="intro-vic20.prg"
 VIC20_OUT="sdrrvic"
@@ -12,7 +15,8 @@ DISK_NAME="sdrr.d64"
 
 rm *.prg *.d64 2> /dev/null
 $PETCAT -w2 -o $C64_PRG -- $C64_BAS
+$PETCAT -w2 -o $C64_PRG2 -- $C64_BAS2
 $PETCAT -w2 -o $VIC20_PRG -- $VIC20_BAS
-$C1541 -format "$DISK_HDR" d64 $DISK_NAME -write $C64_PRG $C64_OUT -write $VIC20_PRG $VIC20_OUT > /dev/null
+$C1541 -format "$DISK_HDR" d64 $DISK_NAME -write $C64_PRG $C64_OUT -write $C64_PRG2 $C64_OUT2 -write $VIC20_PRG $VIC20_OUT > /dev/null
 
 echo "Created disk image: $DISK_NAME"
